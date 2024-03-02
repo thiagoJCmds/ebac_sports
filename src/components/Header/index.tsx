2a288a3cc31f24e44e440fc966188ad06eec9a01
Thiago Manoel da Silva <thiagomds03@gmail.com>
2024-03-02 15:11:07 -0300

    redux

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,14 +4,16 @@ import { Produto } from '../../App'
 
 import cesta from '../../assets/cesta.png'
 import { paraReal } from '../Produto'
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 
 type Props = {
-  itensNoCarrinho: Produto[]
   favoritos: Produto[]
 }
 
-const Header = ({ itensNoCarrinho, favoritos }: Props) => {
-  const valorTotal = itensNoCarrinho.reduce((acc, item) => {
+const Header = ({ favoritos }: Props) => {
+  const itens = useSelector((state: RootReducer) => state.carrinho.itens)
+  const valorTotal = itens.reduce((acc: any, item: { preco: any }) => {
     acc += item.preco
     return acc
   }, 0)
@@ -23,7 +25,7 @@ const Header = ({ itensNoCarrinho, favoritos }: Props) => {
         <span>{favoritos.length} favoritos</span>
         <img src={cesta} />
         <span>
-          {itensNoCarrinho.length} itens, valor total: {paraReal(valorTotal)}
+          {itens.length} itens, valor total: {paraReal(valorTotal)}
         </span>
       </div>
     </S.Header>
--- src/components/Produto/index.tsx
+++ src/components/Produto/index.tsx
@@ -1,9 +1,11 @@
-import { Produto as ProdutoType } from '../../App'
 import * as S from './styles'
+import { Produto as ProdutoType } from '../../App'
+import { adicionar } from '../../store/reducers/carrinho'
+import { useDispatch } from 'react-redux'
 
 type Props = {
   produto: ProdutoType
-  aoComprar: (produto: ProdutoType) => void
+
   favoritar: (produto: ProdutoType) => void
   estaNosFavoritos: boolean
 }
@@ -12,10 +14,10 @@ export const paraReal = (valor: number) =>
   new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
     valor
   )
-
+const dispacth = useDispatch()
 const ProdutoComponent = ({
   produto,
-  aoComprar,
+
   favoritar,
   estaNosFavoritos
 }: Props) => {
@@ -33,7 +35,7 @@ const ProdutoComponent = ({
           ? '- Remover dos favoritos'
           : '+ Adicionar aos favoritos'}
       </S.BtnComprar>
-      <S.BtnComprar onClick={() => aoComprar(produto)} type="button">
+      <S.BtnComprar onClick={() => dispacth(adicionar(produto))} type="button">
         Adicionar ao carrinho
       </S.BtnComprar>
     </S.Produto>
